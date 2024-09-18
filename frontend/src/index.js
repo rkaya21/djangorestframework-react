e7fc1e3f288546e8feeb6a884befe02519575704
Recep Enes Kaya <recepenskaya@gmail.com>
2024-09-19 00:05:53 +0300

    refactor: add footer component and root rendering

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
-import './index.css';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import App from './App';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import "./index.css";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import App from "./App";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const routing = (
   <Router>
-      <React.StrictMode>
-          <Header />
-          <Switch>
-                <Route exact path="/" component={App} />
-          </Switch>
-      </React.StrictMode>
+    <React.StrictMode>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+      <Footer />
+    </React.StrictMode>
   </Router>
 );
+
+root.render(routing);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
