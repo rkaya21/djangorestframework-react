3a323735b918202ef9e66ab91eb1b6910f6be77f
Recep Enes Kaya <recepenskaya@gmail.com>
2024-09-19 20:26:40 +0300

    feat: setup routing with Header and Footer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import App from './App';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+
+const theme = createTheme({
+  // Tema ayarları gelecek
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const routing = (
-  <Router>
-    <React.StrictMode>
-      <Header />
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-      <Footer />
-    </React.StrictMode>
-  </Router>
+  <ThemeProvider theme={theme}>
+    <Router>
+      <React.StrictMode>
+        <Header />
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+        <Footer />
+      </React.StrictMode>
+    </Router>
+  </ThemeProvider>
 );
 
 root.render(routing);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Eğer uygulamanızın çevrimdışı çalışmasını ve daha hızlı yüklenmesini istiyorsanız, aşağıdaki kodu kullanabilirsiniz.
+// unregister() yerine register() yapabilirsiniz. Ancak, bu bazı riskler içerir.
+// Daha fazla bilgi için: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
