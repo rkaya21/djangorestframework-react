a7c14a150899c4e4dd88551585f87cfbc16f437e
Recep Enes Kaya <recepenskaya@gmail.com>
2024-09-24 19:03:37 +0300

    refactor: Add tests posts CRUD operations

--- backend/blog_api/tests.py
+++ backend/blog_api/tests.py
@@ -3,22 +3,25 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from blog.models import Post, Category
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 
 
 class PostTests(APITestCase):
 
     def test_view_posts(self):
-
         url = reverse('blog_api:listcreate')
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def create_post(self):
-        self.test_category = Category.objects.create(name='django')
 
-        self.testuser = User.objets.create_user(
-            user_name='recep', password='123456789'
+    def test_create_post(self):
+        self.test_category = Category.objects.create(name='django')
+        self.testuser = User.objects.create_superuser(
+            username='recep', password='123456789'
         ) 
+        self.client.login(username=self.testuser.username, password='123456789')
+
+
         data = {
             "title": "new",
             "author": 1,
@@ -28,3 +31,35 @@ class PostTests(APITestCase):
         url = reverse('blog_api:listcreate')
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+
+    def test_post_update(self):
+        client = APIClient()
+
+        self.test_category = Category.objects.create(name='django')
+        self.testuser1 = User.objects.create_user(username='test_user1', password='123')
+        self.testuser2 = User.objects.create_user(username='test_user2', password='456')
+
+        test_post = Post.objects.create(
+            category_id=1,
+            title='Post Title',
+            excerpt='Post Excerpt',
+            content='Post Content',
+            slug ='post-title',
+            author=self.testuser1,
+            status='published'
+        )
+
+        client.login(username=self.testuser1.username, password='123')
+        url = reverse(('blog_api:detailcreate'), kwargs={'pk': 1})
+
+        response = client.put(
+            url, {
+                "title": "New",
+                "author": self.testuser1.id,
+                "excerpt": "New",
+                "content": "New",
+                "status": "published"
+            }, format="json")
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
