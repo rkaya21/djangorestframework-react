27a99f3bc7719ba56f830b11fe7d436c899178dd
Recep Enes Kaya <recepenskaya@gmail.com>
2024-09-30 20:15:38 +0300

    style(footer): improve footer layout and styling with Box component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -14,36 +14,47 @@ const footers = [
 
 function Footer() {
   return (
-    <Container maxWidth="md" component="footer">
-      <Grid container spacing={4} justifyContent="space-evenly">
-        {footers.map((footer) => (
-          <Grid item xs={6} sm={3} key={footer.title}>
-            <Typography variant="h6" color="textPrimary" gutterBottom>
-              {footer.title}
-            </Typography>
-            <ul>
-              {footer.description.map((item) => (
-                <li key={item}>
-                  <Link href="#" variant="subtitle1" color="textSecondary">
-                    {item}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </Grid>
-        ))}
-      </Grid>
-      <Box mt={5}>
-        <Typography variant="body2" color="text.secondary" align="center">
-          {'Telif hakkı © '}
-          <Link color="inherit" href="https://mui.com/">
-            Web Siteniz
-          </Link>{' '}
-          {new Date().getFullYear()}
-          {'.'}
-        </Typography>
-      </Box>
-    </Container>
+    <Box
+      component="footer"
+      sx={{
+        py: 3,
+        px: 2,
+        mt: 'auto',
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+      }}
+    >
+      <Container maxWidth="md">
+        <Grid container spacing={4} justifyContent="space-evenly">
+          {footers.map((footer) => (
+            <Grid item xs={6} sm={3} key={footer.title}>
+              <Typography variant="h6" color="textPrimary" gutterBottom>
+                {footer.title}
+              </Typography>
+              <ul style={{ listStyle: 'none', padding: 0 }}>
+                {footer.description.map((item) => (
+                  <li key={item}>
+                    <Link href="#" variant="subtitle1" color="textSecondary">
+                      {item}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </Grid>
+          ))}
+        </Grid>
+        <Box mt={5}>
+          <Typography variant="body2" color="textSecondary" align="center">
+            {'Telif hakkı © '}
+            <Link color="inherit" href="https://mui.com/">
+              Web Siteniz
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
   );
 }
 
